59e37eae77d8ff9f69bfd037c8bbfc4d982c891f
tjclawson <tjamesclawson@gmail.com>
2020-05-13 22:24:26 -0400

    Add logic to have main activity launch correct layout

--- SpaceX/app/src/main/java/com/tjclawson/spacex/ui/MainActivity.kt
+++ SpaceX/app/src/main/java/com/tjclawson/spacex/ui/MainActivity.kt
@@ -1,7 +1,10 @@
-package com.tjclawson.spacex
+package com.tjclawson.spacex.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.tjclawson.spacex.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,9 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_container, LaunchListFragment())
-            .commit()
+        val viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+
+        // Sets twoPane to true if in landscape, main_container will be null if activity_main (land)
+        // layout is inflated
+        viewModel.twoPane = main_container == null
+
+        // Replaces appropriate layout based on screen orientation
+        if (viewModel.twoPane) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame_item_list, LaunchListFragment())
+                .replace(R.id.frame_item_detail, LaunchDetailFragment())
+                .commit()
+        } else {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_container, LaunchListFragment())
+                .commit()
+        }
     }
 }
